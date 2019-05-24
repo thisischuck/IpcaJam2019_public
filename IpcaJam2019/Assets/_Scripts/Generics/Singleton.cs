fb2334640a5b5fe07640fc241cca6f909e172019
Unknown <j.lucas12@hotmail.com>
2019-05-25 00:28:16 +0100

    Sound Manager

---
+++ IpcaJam2019/Assets/_Scripts/Generics/Singleton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Component
+{
+    public static T Instance;
+
+    protected virtual void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this as T;
+        }
+        else
+        {
+            Debug.Log("Multiple Objects " + typeof(T).Name);
+            Destroy(this.gameObject);
+            return;
+        }
+    }
+}
