6c13a6fd0d4e05a40ad286177af3b309c6f9f596
xfilipefx <xfilipefx@gmail.com>
2019-05-25 23:59:52 +0100

    UI part1

---
+++ IpcaJam2019/Assets/Scripts/UI/ButtonController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonController : MonoBehaviour
+{
+    private Button button;
+    public LevelObject levelInfo;
+    private GameObject txtChild;
+
+    private TMP_Text levelTxt;
+
+    public Color color;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(TaskOnClick);
+        txtChild = transform.Find("txtLv").gameObject;
+        levelTxt = txtChild.GetComponent<TMP_Text>();
+
+        if (!levelInfo.locked)
+            levelTxt.text = levelInfo.num.ToString();
+        else
+            levelTxt.text = "?";
+    }
+
+    public void TaskOnClick()
+    {
+        if(!levelInfo.locked)
+            SceneManager.LoadScene(levelInfo.levelName);
+    }   
+
+    // Update is called once per frame
+    void Update()
+    {
+        //HighlightButton();
+        if (!levelInfo.locked)
+            levelTxt.text = levelInfo.num.ToString();
+        else
+            levelTxt.text = "?";
+    }
+
+    public void Highlight()
+    {
+        levelTxt.color = color;
+    }
+
+    public void DeHighlight()
+    {
+        levelTxt.color = Color.black;
+    }
+}
---
+++ IpcaJam2019/Assets/Scripts/UI/MenuController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    public List<LevelObject> levels;
+    public Slider slider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LevelsController.numLevels = 1;
+        LevelsController.Start();
+        if (slider != null)
+            slider.value = LevelsController.volume;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (levels.Count > 0)
+        {
+            for (int i = 0; i < levels.Count; i++)
+            {
+                if (LevelsController.levels[i] == 0)
+                {
+                    levels[i].locked = false;
+                }
+                else if (LevelsController.levels[i] == 1)
+                {
+                    levels[i].locked = true;
+                }
+                        
+            }
+        }
+        if (slider != null)
+            LevelsController.volume = slider.value;
+    }
+
+    public void GotoLevelSelect()
+    {
+        SceneManager.LoadScene("LevelSelect");
+    }
+
+    public void GotoMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
