f306ea073504655ade86977649d76aa9d0098964
Unknown <j.lucas12@hotmail.com>
2019-05-26 01:27:47 +0100

    btn

--- IpcaJam2019/Assets/_Scripts/Mechanics/ButtonBehaviour.cs
+++ IpcaJam2019/Assets/_Scripts/Mechanics/ButtonBehaviour.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Button : MonoBehaviour
+public class ButtonBehaviour : MonoBehaviour
 {
     public enum ButtonType { Normal, Duration, Hold }
 
